f6a44bb9f0959b318841bf18c7f91a3e096ba34d
Muhammad Vikri <muhammadvikri@MacBook-Pro-Muhammad.local>
2021-07-01 21:01:03 +0700

    Final ver 1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            { name: 'ICON-IBOX Mobile Flutter', class: "flutter", img: "img/project/project1.png", link: "http://kbmpti.filkom.ub.ac.id/"}
+        ]
+    }
+})
